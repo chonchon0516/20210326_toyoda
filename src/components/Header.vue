a6820a6fc94d4b0e1ac2c1c8e7fc0659990757b3
chonchon0516 <azumi.toyoda@gmail.com>
2021-03-28 09:15:48 +0900

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="center">
+      <p class="header-logo">Weather App</p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+.center {
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    views: Home,
+  },
+  {
+    path: "/home/:postcode",
+    name: "Home",
+    views: Home,
+    props: true,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
